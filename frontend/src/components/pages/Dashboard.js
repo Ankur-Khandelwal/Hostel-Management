9a2bac02452fe3e9a3b67ac27dd94dd672fa52e1
Anmol Panda <anmolpanda1908@gmail.com>
2021-11-29 12:18:20 +0530

    modified dashboard

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -6,12 +6,12 @@ import { getCurrentUser } from "../../actions/authActions";
 
 // const clean = require("../../img/cleaning.jpg");
 const student = require("../../img/student.jpg");
-const staff = require("../../img/staff.jpeg");
+const staff = require("../../img/staff.jpg");
 const bedRoom = require("../../img/bedroom.jpeg");
 class Dashboard extends Component {
   componentDidMount() {
     if (!this.props.auth.isAuthenticated) {
-      this.props.history.push("/");
+      // this.props.history.push("/");
     }
   }
 
@@ -20,21 +20,22 @@ class Dashboard extends Component {
     return (
       <div className="mid container">
         <div className="text-center" style={{ fontSize: "25px" }}>
-          Welcome {user.name}!
+          <h3>Welcome {user.name}!</h3>
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '2rem' }}>
 
-          <div className="card hoverable" style={{ width: "18rem", hover: '', height: '22rem' }}>
+          <div className="card hoverable" style={{ width: "30rem", hover: '', height: 'auto', border: '2px solid'}}>
             <img src={student} className="card-img-top" alt="Cleaning" />
             <div className="card-body" style={{ height: '10rem' }}>
-              <h5 className="card-title">Student</h5>
-              <a href="/student" className="card-text">
-                Add new Student and allot Room or Check Info
-              </a>
+              <center><h5 className="card-title">Student</h5>
+              <p>Click below to Add new Student and alot Room or Check Info</p>
+              <a href="/student"  className="btn btn-primary">
+                Add
+              </a></center>
             </div>
           </div>
 
-          <div className="card" style={{ width: "18rem", hover: '', height: '22rem' }}>
+          {/* <div className="card" style={{ width: "18rem", hover: '', height: '22rem' }}>
             <img src={bedRoom} className="card-img-top" alt="Cleaning" />
             <div className="card-body">
               <h5 className="card-title">Room Repair/Cleaning Status</h5>
@@ -42,15 +43,17 @@ class Dashboard extends Component {
                 Add Room Repair/Cleaning or Check Info
                 </a>
             </div>
-          </div>
+          </div> */}
 
-          <div className="card" style={{ width: "18rem", hover: '', height: '22rem' }}>
+          <div className="card" style={{ width: "30rem", hover: '', height: 'auto',border: '2px solid' }}>
             <img src={staff} className="card-img-top" alt="Cleaning" />
             <div className="card-body">
-              <h5 className="card-title">Staff Info</h5>
-              <a href="/staff">
-                Add more Staff or Check their info
+              <center><h5 className="card-title">Staff Info</h5>
+              <p>Click below to Add more Staff or Check their info</p>
+              <a href="/staff" className=" btn btn-primary">
+                Add 
               </a>
+              </center>
             </div>
           </div>
 
